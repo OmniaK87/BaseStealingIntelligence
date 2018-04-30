f3d44fc471347f98e2b6c65df42dc0840d3738da
Omniak87 <jake.mitchell@colorado.edu>
2018-04-30 09:30:29 -0600

    Changed matplotlib backend

--- BaseStealingIntelligence.py
+++ BaseStealingIntelligence.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
 
 from sklearn.metrics import r2_score
 from flask import Flask, render_template, send_file
